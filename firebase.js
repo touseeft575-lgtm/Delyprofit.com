3b378c2b941467ce3d0282c0a68b90c8df47432a
touseeft575-lgtm <touseeft575@gmail.com>
2026-05-10 15:12:20 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -13,16 +13,27 @@ getStorage
 } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js";
 
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_AUTH_DOMAIN",
-projectId: "YOUR_PROJECT_ID",
-storageBucket: "YOUR_STORAGE_BUCKET",
-messagingSenderId: "YOUR_MSG_ID",
-appId: "YOUR_APP_ID"
+
+apiKey: "AIzaSyBLygZa2EDyHBuCT8RTZEMIcnvYW-D-67g",
+
+authDomain: "dailyprofit-b9600.firebaseapp.com",
+
+projectId: "dailyprofit-b9600",
+
+storageBucket: "dailyprofit-b9600.firebasestorage.app",
+
+messagingSenderId: "879891413676",
+
+appId: "1:879891413676:web:bfba99ee5b95c45b957d09",
+
+measurementId: "G-T5FJ81FP62"
+
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
 export const auth = getAuth(app);
+
 export const storage = getStorage(app);
